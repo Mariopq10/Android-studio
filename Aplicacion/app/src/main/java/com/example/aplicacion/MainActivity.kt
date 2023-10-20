e61a7cda85a3ae0c1477c2b61616a7a12c250927
Mario PQ <115182975+Mariopq10@users.noreply.github.com>
2023-10-20 18:54:37 +0200

    correccion

--- Aplicacion/app/src/main/java/com/example/aplicacion/MainActivity.kt
+++ Aplicacion/app/src/main/java/com/example/aplicacion/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.aplicacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        btnStart.setOnClickListener{
+            Log.i("Mario", "Button")
+        }
     }
 }
