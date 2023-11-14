5675ba8e398e1873c42f4e51476017790c9ee26a
Mario PQ <115182975+Mariopq10@users.noreply.github.com>
2023-11-14 19:34:18 +0100

    ejercicio

--- Cinempq/app/src/main/java/com/example/cinempq/AboutActivity.kt
+++ Cinempq/app/src/main/java/com/example/cinempq/AboutActivity.kt
@@ -1,12 +1,19 @@
 package com.example.cinempq
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.security.KeyChain.EXTRA_NAME
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.cinempq.databinding.ActivityMainBinding
 
 class AboutActivity : AppCompatActivity() {
+    val NAME_REQUEST=1
 
     private lateinit var binding: ActivityMainBinding
 
@@ -16,20 +23,24 @@ class AboutActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         var infoAcerca = "Esto es un Snackbar de informacion"
+
         var toastButton : Button = findViewById(R.id.supportButton)
         binding.supportButton.setOnClickListener{
-            Toast.makeText(this,R.string.infoToast,Toast.LENGTH_LONG).show()
+            val direccionCorreo = "mario.pq10@gmail.com"
+            val uri = Uri.parse("mailto:$direccionCorreo")
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+            startActivity(intent)
         }
         var toastButton2 : Button = findViewById(R.id.webButton)
         binding.webButton.setOnClickListener{
-            Toast.makeText(this,R.string.infoToast,Toast.LENGTH_LONG).show()
+            val uri = Uri.parse("http://www.google.com")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
         }
         var toastButton3 : Button = findViewById(R.id.exitButton)
-        binding.exitButton.setOnClickListener{
-            Toast.makeText(this,R.string.infoToast,Toast.LENGTH_LONG).show()
+        binding.exitButton.setOnClickListener{finish()
         }
 
-
-
     }
+
 }
