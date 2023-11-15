86f608093cb2d9736a95cbd27aea4f8465feb7d4
Mario PQ <115182975+Mariopq10@users.noreply.github.com>
2023-11-15 20:55:22 +0100

    a

---
+++ Cinempq/app/src/main/java/com/example/cinempq/FilmEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cinempq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_edit)
+    }
+}
