d239db25c67f4a28bd666f961ecbf459cbc1f0eb
Mariopq10 <115182975+Mariopq10@users.noreply.github.com>
2024-03-05 22:38:51 +0100

    fin proyecto

---
+++ Runas-main/app/src/main/java/com/example/runas/Login/Registro.kt
@@ -0,0 +1,74 @@
+package com.example.runas.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.runas.DBControler.Usuario
+import com.example.runas.DBControler.UsuarioDatabase
+import com.example.runas.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.launch
+
+class Registro : AppCompatActivity() {
+    lateinit var database: UsuarioDatabase
+    lateinit var guardar: Button
+    lateinit var cancelar: Button
+    lateinit var user: EditText
+    lateinit var pass: EditText
+    lateinit var passAgain: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+        database = UsuarioDatabase(this)
+
+        guardar = findViewById(R.id.btnGuardar)
+        cancelar = findViewById(R.id.btnCancelar)
+        user = findViewById(R.id.editTextUser)
+        pass = findViewById(R.id.editTextPass)
+        passAgain = findViewById(R.id.editTextPassAgain)
+
+        guardar.setOnClickListener {
+
+            if (comprobarContra(pass.text.toString(), passAgain.text.toString())) {
+                showToast("Las contraseñas coinciden.")
+                val usuario = Usuario(usuario = user.text.toString(), contrasenya = pass.text.toString())
+                // Lanzar una corrutina en el hilo de fondo
+                CoroutineScope(Dispatchers.IO).launch {
+                    // Llamar a insertUsuarioSafe en el hilo de fondo
+                    val exitoso = database.usuarioDao().insertUsuarioSafe(usuario)
+
+                    // Actualizar la interfaz de usuario en el hilo principal
+                    withContext(Dispatchers.Main) {
+                        if (exitoso) {
+                            showToast("Usuario insertado correctamente.")
+                            finish()
+                        } else {
+                            showToast("Error al insertar el usuario.")
+                        }
+                    }
+                }
+            } else {
+                showToast("Las contraseñas no coinciden.")
+            }
+        }
+
+        cancelar.setOnClickListener {
+            if(!isFinishing){
+                finish()
+            }
+        }
+    }
+
+    private fun comprobarContra(pass:String, passAgain:String):Boolean{
+        return pass == passAgain
+    }
+
+    private fun showToast(text:String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ TodoList/app/src/main/java/com/example/todolisttutorial/TaskViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.todolisttutorial
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+
+class TaskViewModel(private val repository: TaskItemRepository): ViewModel()
+{
+    val taskItems: LiveData<List<TaskItem>> = repository.allTaskItems.asLiveData()
+
+    fun addTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.insertTaskItem(taskItem)
+    }
+
+    fun updateTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.updateTaskItem(taskItem)
+    }
+    fun deleteTaskItem(taskItem: TaskItem) = viewModelScope.launch {
+        repository.deleteTaskItem(taskItem)
+    }
+
+    fun setCompleted(taskItem: TaskItem) = viewModelScope.launch {
+        if (!taskItem.isCompleted())
+            taskItem.completedDateString = TaskItem.dateFormatter.format(LocalDate.now())
+        repository.updateTaskItem(taskItem)
+    }
+    fun setUncompleted(taskItem: TaskItem)= viewModelScope.launch {
+        if(taskItem.isCompleted())
+            taskItem.completedDateString
+    }
+}
+
+class TaskItemModelFactory(private val repository: TaskItemRepository) : ViewModelProvider.Factory
+{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T
+    {
+        if (modelClass.isAssignableFrom(TaskViewModel::class.java))
+            return TaskViewModel(repository) as T
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
